cbb3673459a00a0720a56bec2c1a073597f79e3b
Elias Moreira <emoreiravirtus@gmail.com>
2022-02-14 18:17:13 -0300

    Project setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContractsComponent } from './contracts/contracts.component';
+
+const routes: Routes = [
+  { path: '', component: ContractsComponent },
+  { path: 'contracts', component: ContractsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
